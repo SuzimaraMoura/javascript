f4f89b0a1aa2ea27d6d28370f48565a1a2ab70ed
SuzimaraMoura <112523571+SuzimaraMoura@users.noreply.github.com>
2023-07-06 23:05:43 -0300

    Repetições em JavaScript

---
+++ cursojs/aula14/ambiente.js
@@ -0,0 +1,19 @@
+/*var c = 1
+while (c<=5){
+    console.log(c)
+    c=c+1   // Pode ser substituido por c++ ou c+=1
+}*/
+//---------------------------------------------------------
+/*var c = 1
+do{
+    console.log(c)
+    c++
+} while (c <= 5) */
+//---------------------------------------------------------
+console.log('Começa aqui')
+for( var c = 5; c<=8; c++ ){
+    console.log(c)
+}
+console.log('FIM...')
+
+   
---
+++ cursojs/aula14/node_71f69fee0a118.tmp.js
@@ -0,0 +1,9 @@
+var c = 1
+/*while (c<=5){
+    console.log(c)
+    c=c+1   // Pode ser substituido por c++ ou c+=1
+}*/
+do{
+    console.log(c)
+}while (c <= 5) 
+    c++
